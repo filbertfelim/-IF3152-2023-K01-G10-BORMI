ae120ba919da0c14eafd40d4f780ed786712a84e
jessicaaall <18221073@std.stei.itb.ac.id>
2023-11-05 20:04:28 +0700

    feat: add transaction api

--- src/server/api/routers/transaction.ts
+++ src/server/api/routers/transaction.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
@@ -14,8 +15,8 @@ export const transactionRouter = createTRPCRouter({
             const data = await ctx.db.transaction.findMany({
                 where: {
                     date: {
-                        gte: input.startDate,
-                        lte: input.endDate,
+                        gte: input.startDate ? input.startDate: undefined,
+                        lte: input.endDate ? input.endDate: undefined,
                     },
                 },
                 take: 10,
@@ -25,11 +26,18 @@ export const transactionRouter = createTRPCRouter({
                 },
             });
 
+            if (data.length === 0) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "No transaction data available",
+                });
+            }
+
             const count = await ctx.db.transaction.count({
                 where: {
                     date: {
-                        gte: input.startDate,
-                        lte: input.endDate,
+                        gte: input.startDate ? input.startDate: undefined,
+                        lte: input.endDate ? input.endDate: undefined,
                     },
                 },
             });
@@ -37,6 +45,53 @@ export const transactionRouter = createTRPCRouter({
             return {
                 data,
                 totalPage: Math.ceil(count / 10),
+            };
+        }),
+    
+    addTransactions: protectedProcedure
+        .input(
+            z.object({
+                userId: z.number(),
+                date: z.date(),
+            }),
+        )
+        .mutation(async ({ ctx, input }) => {
+            let transaction;
+
+            try {
+                transaction = await ctx.db.transaction.create({
+                    data: {
+                        userId: input.userId,
+                        date: input.date,
+                    },
+                });
+            } catch (error) {
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Failed to add transaction",
+                });
+            }
+
+            try {
+                await ctx.db.cartItem.updateMany({
+                    where: {
+                        userId: input.userId,
+                        isPurchased: false,
+                    },
+                    data: {
+                        isPurchased: true,
+                        transactionId: transaction.id,
+                    },
+                });
+            } catch (error) {
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Failed to update cart item",
+                });
+            }
+
+            return {
+                message: "Checkout successful",
             };
         }),
 });
